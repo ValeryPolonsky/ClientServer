4bcd130de38a49821df52dabf25e5a9144166fc7
Valery.Polonsky <Valery.Polonsky@bagirasys.com>
2024-08-06 09:47:12 +0300

    Server - commit 1

--- Server/Server/Controllers/HomeController.cs
+++ Server/Server/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Server.Models;
 using System.Diagnostics;
 
 namespace Server.Controllers
@@ -11,23 +10,7 @@ namespace Server.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }        
 
         [HttpGet]
         [Route("/[controller]/GetData")]
