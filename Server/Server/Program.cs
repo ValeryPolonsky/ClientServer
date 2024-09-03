ce35ec934370fc4f9ead757771025c06a0a03559
Valery.Polonsky <Valery.Polonsky@bagirasys.com>
2024-09-03 14:18:28 +0300

    ClientServer - commit 7

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CompanyManager API",
+        Version = "v1",
+        Description = "Northwind API"
+    });
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -15,6 +30,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI(options => 
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseRouting();
 
